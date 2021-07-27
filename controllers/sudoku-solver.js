ecdbe667a09b250afd93df80a99b100b0c176468
eastraining <21341281+eastraining@users.noreply.github.com>
2021-07-27 22:14:56 +0800

    coded initial tests and solver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,171 @@
 class SudokuSolver {
+  constructor(gridSide = 9) {
+    this.GRID_SIDE = gridSide;
+    this.REGION_SIDE = Math.floor(Math.sqrt(gridSide));
+  }
+
+  rowToIdx(char) {
+    return typeof char === 'number' ?
+    char : Number(char.charCodeAt() - 'A'.charCodeAt());
+  }
+  colToIdx(num) {
+    return typeof num === 'number' ?
+    num : Number(num - 1);
+  }
+  
 
   validate(puzzleString) {
+    return [puzzleString.length === 81, /^[\d.]+$/.test(puzzleString)];
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  checkRowPlacement(puzzleString, row, column, value, verify = true) {
+    const rowIdx = this.rowToIdx(row);
+    const colIdx = this.colToIdx(column);
+    const rowOfVal = rowIdx * this.GRID_SIDE;
+    const rowEnd = (rowIdx + 1) * this.GRID_SIDE;
+    const valPosition = rowOfVal + colIdx;
+    const testVals = puzzleString.slice(rowOfVal, valPosition)
+    .concat(puzzleString.slice(valPosition + 1, rowEnd));
+    return verify ? !testVals.includes(String(value)) : testVals;
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-
+  checkColPlacement(puzzleString, row, column, value, verify = true) {
+    const rowIdx = this.rowToIdx(row);
+    const colIdx = this.colToIdx(column);
+    const rowOfVal = rowIdx * this.GRID_SIDE;
+    const valPosition = rowOfVal + colIdx;
+    let testVals = ""; 
+    for (let i = 0, n = this.GRID_SIDE; i < n; i++) {
+      let currIdx = i * this.GRID_SIDE + colIdx;
+      testVals = currIdx === valPosition ? testVals :
+      testVals.concat(puzzleString.charAt(currIdx));
+    }
+    return verify ? !testVals.includes(String(value)) : testVals;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
+  checkRegionPlacement(puzzleString, row, column, value, verify = true) {
+    const rowIdx = this.rowToIdx(row);
+    const colIdx = this.colToIdx(column);
+    const startRow = Math.floor(rowIdx / this.REGION_SIDE) * this.REGION_SIDE;
+    const startCol = Math.floor(colIdx / this.REGION_SIDE) * this.REGION_SIDE;
+    const rowOfVal = rowIdx * this.GRID_SIDE;
+    const valPosition = rowOfVal + colIdx;
+    let testVals = ""; 
+    for (let i = 0, n = this.REGION_SIDE; i < n; i++) {
+      for (let j = 0, m = this.REGION_SIDE; j < m; j++) {
+        let currIdx = startRow + i * this.GRID_SIDE + startCol + j;
+        testVals = currIdx === valPosition ? testVals :
+        testVals.concat(puzzleString.charAt(currIdx));
+      }
+    }
+    if (testVals.length > 8) {
+      console.log(testVals, startRow, startCol, row, rowIdx, column, colIdx, value);
+    }
+    return verify ? !testVals.includes(String(value)) : testVals;
   }
 
   solve(puzzleString) {
+    console.log(puzzleString);
+    let valid = this.validate(puzzleString);
+    if (valid.includes(false)) {
+      console.log(puzzleString, valid);
+      return valid;
+    }
     
+    
+    const checkNum = (puzzleString, row, column, value, verify = true) => {
+      const args = [puzzleString, row, column, value, verify];
+      return [
+        this.checkRowPlacement(...args),
+        this.checkColPlacement(...args),
+        this.checkRegionPlacement(...args)
+      ];
+    }
+
+    if (/^\d+$/.test(puzzleString)) {
+      console.log(puzzleString);
+      for (let idx = 0, n = puzzleString.length; idx < n; idx++) {
+        let char = puzzleString[idx];
+        const rowIdx = Math.floor(idx / 9);
+        const colIdx = idx % 9;
+        let result = checkNum(puzzleString, rowIdx, colIdx, char);
+        if (result.includes(false)) {
+          return false;
+        }
+      }
+      return puzzleString;
+    } else {
+      // structure of each missing number shall be:
+      // [ idx, [ possible numbers for the cell at that index... ] ] 
+      let missingNums = [];
+      
+      let solvedString = puzzleString.slice();
+      function updateSolved(idx, char) {
+        solvedString = solvedString.slice(0, idx).concat(char)
+        .concat(solvedString.slice(idx + 1));
+      }
+      
+      for (let idx = 0, n = puzzleString.length; idx < n; idx++) {
+        let char = puzzleString[idx];
+        if (char !== '.') {
+          continue;
+        } else {
+          let currCell = [idx, []];
+          const rowIdx = Math.floor(idx / 9);
+          const colIdx = idx % 9;
+          const result = checkNum(solvedString, rowIdx, colIdx, char, false).join('');
+          for (let j = 1, m = 9; j <= m; j++) {
+            if(!result.includes(String(j))) {
+              currCell[1].push(String(j));
+            }
+          }
+          if (currCell[1].length === 0) {
+            console.log(currCell, currCell[1], result);
+            return false;
+          } else if (currCell[1].length === 1) {
+            updateSolved(idx, currCell[1][0]);
+          } else {
+            missingNums.push(currCell);
+          }
+        }
+      };
+
+      missingNums.sort((x, y) => {
+        return x[1].length - y[1].length; 
+      });
+
+      const checkCell = (missingIdx = 0) => { 
+        console.log(missingIdx);
+        if (missingIdx < 0) {
+          return false;
+        } else if (missingIdx === missingNums.length) {
+          console.log(missingIdx, solvedString);
+          return /^\d{81}$/.test(solvedString) ? solvedString : false;
+        } else {
+          const currCell = missingNums[missingIdx];
+          const puzzleIdx = currCell[0];
+          const rowIdx = Math.floor(puzzleIdx / GRID_SIDE);
+          const colIdx = puzzleIdx % GRID_SIDE;
+          const tryNums = currCell[1];
+          const puzzleVal = solvedString[puzzleIdx];
+          const tryNumStartIdx = tryNums.indexOf(puzzleVal) + 1;
+          for (let i = tryNumStartIdx, n = tryNums.length; i < n; i++) {
+            let tryNum = tryNums[i];
+            let result = checkNum(solvedString, rowIdx, colIdx, tryNum);
+            if (result.includes(false)) {
+              continue;
+            } else {
+              updateSolved(puzzleIdx, tryNum);
+              return checkCell(missingIdx + 1);
+            }
+          }
+          updateSolved(puzzleIdx, '.');
+          return checkCell(missingIdx - 1);
+        }
+      }
+
+      return checkCell(0);
+    }
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-const puzzleStrings = require('../controllers/puzzle-strings.js');
+import { puzzlesAndSolutions } from '../controllers/puzzle-strings.js';
 let solver = new Solver();
 
 suite('UnitTests', () => {
@@ -10,23 +10,64 @@ suite('UnitTests', () => {
   suite('Tests for validate method', function() {
     // #1
     test('Logic handles a valid puzzle string of 81 characters', function() {
-      assert.isOk(solver.validate(puzzleStrings[0][0]), '81-character string of 1-9 and . is ok');
+      assert.sameOrderedMembers(solver.validate(puzzlesAndSolutions[0][0]), [true, true]);
     });
     // #2
     test('Logic handles a puzzle string with invalid characters', function() {
-      assert.isNotOk(solver.validate(puzzleStrings[1][0].slice(0, 80).concat('a')), '81-character string which includes non 1-9 or . is not ok');
+      assert.sameOrderedMembers(solver.validate(puzzlesAndSolutions[1][0].slice(0, 80).concat('a')), [true, false]);
     });
     // #3
     test('Logic handles a puzzle string that is not 81 characters in length', function() {
-      assert.isNotOk(solver.validate('123456.3345345...123'), 'Non-81-character string is not ok');
+      assert.sameOrderedMembers(solver.validate('123456.3345345...123'), [false, true]);
     });
   });
 
   suite('Tests for checkRowPlacement method', function() {
     // #4
     test('Logic handles a valid row placement', function() {
-      assert.isOk(solver.checkRowPlacement())
-    })
-  })
+      assert.isOk(solver.checkRowPlacement(puzzlesAndSolutions[0][0], 'A', '2', '3'));
+    });
+    // #5
+    test('Logic handles an invalid row placement', function() {
+      assert.isNotOk(solver.checkRowPlacement(puzzlesAndSolutions[2][0], 'A', '1', '9'));
+    });
+  });
 
+  suite('Tests for checkColPlacement method', function() {
+    // #6
+    test('Logic handles a valid column placement', function() {
+      assert.isOk(solver.checkColPlacement(puzzlesAndSolutions[1][0], 'A', '3', '8'));
+    });
+    // #7
+    test('Logic handles an invalid column placement', function() {
+      assert.isNotOk(solver.checkColPlacement(puzzlesAndSolutions[0][0], 'A', '4', '3'));
+    });
+  });
+
+  suite('Tests for checkRegionPlacement method', function() {
+    // #8
+    test('Logic handles a valid region (3x3 grid) placement', function() {
+      assert.isOk(solver.checkRegionPlacement(puzzlesAndSolutions[4][0], 'A', '3', '7'));
+    });
+    // #9
+    test('Logic handles an invalid region (3x3 grid) placement', function() {
+      assert.isNotOk(solver.checkRegionPlacement(puzzlesAndSolutions[4][0], 'A', '3', '1'));
+    });
+  });
+
+  suite('Tests for solve method', function() {
+    // #10
+    test('Valid puzzle strings pass the solver', function() {
+      assert.equal(solver.solve(puzzlesAndSolutions[3][1]), puzzlesAndSolutions[3][1]);
+    });
+    // #11
+    test('Invalid puzzle strings fail the solver', function() {
+      assert.sameOrderedMembers(solver.solve(puzzlesAndSolutions[3][1].slice(0, 80).concat('a')), [true, false]);
+    });
+    // #12
+    test('Solver returns the expected solution for an incomplete puzzle', 
+    function() {
+      assert.equal(solver.solve(puzzlesAndSolutions[3][0]), puzzlesAndSolutions[3][1]);
+    }); 
+  });
 });
